879c3e54d1eaffbf34d0b12ba4c1e86e9cd012aa
samsif <mohamed.samet@sifast.com>
2020-11-12 13:43:37 +0100

    add server express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hellos world!');
+import {app} from './server/server'
+import {AddressInfo} from 'net'
+
+console.log('Lunch backend app');
+
+const server = app.listen(8099, '0.0.0.0', () => {
+  const {port, address} = server.address() as AddressInfo;
+  console.log('Server listening on:','http://' + address + ':'+port);
+});
